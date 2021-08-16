7c5cb5fc27e350b6165d3850e50381fd2162860a
Babacar Cissé DIA <bcdbuddy@outlook.com>
2021-08-16 11:31:37 +0000

    harden

--- idea/intellij_trial_reset.sh
+++ idea/intellij_trial_reset.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+CONFIG_PATH=~/.config/JetBrains/IntelliJIdea2021.1
+# Old config path : ~/.config/JetBrains/IntelliJIdea*
 echo "removing evaluation key"
-rm -rf ~/IntelliJIdea*/config/eval
-rm -rf ~/.config/JetBrains/IntelliJIdea*/eval
+rm -rf "$CONFIG_PATH/eval"
 
 
 echo "resetting evalsprt in options.xml"
-sed -i '/evlsprt/d' ~/.IntelliJIdea*/config/options/options.xml
+sed -i '/evlsprt/d' "$CONFIG_PATH/options/options.xml"
 
 echo "removing key"
-sed -i '/evlsprt/d' ~/.IntelliJIdea*/config/idea13.key
+sed -i '/evlsprt/d' "$CONFIG_PATH/idea13.key"
 
 echo "resetting evalsprt in other.xml"
-sed -i '/evlsprt/d' ~/.IntelliJIdea*/config/options/other.xml
+sed -i '/evlsprt/d' "$CONFIG_PATH/options/other.xml"
 
 echo "resetting evalsprt in prefs.xml"
 # sed -i '/evlsprt/d' ~/.java/.userPrefs/jetbrains/idea/prefs.xml
 echo "just remove the whole directory java user prefs"
 ## Need to double check if there is anything inside that needs
 ##   to be taken care of. Example: google directory
-rm -fr ~/.java/.userPrefs
+# rm -fr ~/.java/.userPrefs
+
+echo "Removing lock"
+rm -fr ~/.java/.userPrefs/.user.lock*
+echo "Removing prefs"
+rm -fr ~/.java/.userPrefs/prefs.xml
+rm -fr ~/.java/.userPrefs/jetbrains/prefs.xml
 
 echo "Removing consent"
 rm -rf ~/.local/share/JetBrains/consentOptions
