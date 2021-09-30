483e69707fa95f842a1c7c4685676123a32725d2
Babacar Cissé Dia <bcdbuddy@outlook.com>
2021-09-30 17:02:31 +0000

    add header for nginx.laravel.sh

--- dev/nginx/laravel.sh
+++ dev/nginx/laravel.sh
@@ -58,6 +58,14 @@ server {
     access_log $webroot/$name/storage/logs/access.log;
     root $webroot/$name/public;
     error_page 404 /index.php;
+    
+    
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-Content-Type-Options "nosniff";
+    
+    
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
 
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
