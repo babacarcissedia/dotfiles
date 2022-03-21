8ef577d0cd0fb1a150a49011c2a751f0e9e3da2a
Babacar Cissé Dia <bcdbuddy@outlook.com>
2022-03-21 18:31:38 +0000

    Update ssl.sh

--- dev/nginx/ssl.sh
+++ dev/nginx/ssl.sh
@@ -4,5 +4,9 @@ sudo apt-get install -y python3-certbot-nginx
 
 certbot certonly -a manual --rsa-key-size 4096 --email $email -d $domain -d www.$domain
 
-## Might be the renew
-# sudo certbot --nginx -d $domain -d www.$domain
+
+## With base nginx config
+# sudo certbot --nginx --email $email -d $domain -d www.$domain
+
+## Renew command
+# sudo certbot certonly --nginx -d $domain -d www.$domain
