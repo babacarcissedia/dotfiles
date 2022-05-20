f7c5cc937df8e565cc21604187d151232572dd17
Babacar Cissé Dia <bcdbuddy@outlook.com>
2022-05-20 18:55:06 +0000

    Update install.sh

--- dev/php/install.sh
+++ dev/php/install.sh
@@ -2,7 +2,10 @@
 #sudo apt install -y apt-transport-https lsb-release ca-certificates
 #sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 #sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-#sudo apt update
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
 
 # php with some extensions
 PHP_VERSION=${1:-'7.4'}
