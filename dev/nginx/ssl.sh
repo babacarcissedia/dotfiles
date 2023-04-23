8be9514de3f0e370de32037ab294e3a3ba5af5b2
Babacar Cissé Dia <bcdbuddy@outlook.com>
2023-04-23 23:00:18 +0000

    Update ssl.sh

--- dev/nginx/ssl.sh
+++ dev/nginx/ssl.sh
@@ -9,6 +9,11 @@ certbot certonly -a manual --rsa-key-size 4096 --email $email -d $domain -d www.
 # sudo certbot --nginx -d $domain -d www.$domain
 chmod 0755 /etc/letsencrypt/{live,archive}
 
+## iRedAdmin
+mv /etc/ssl/certs/iRedMail.crt{,.bak}
+mv /etc/ssl/private/iRedMail.key{,.bak}
+ln -s "/etc/letsencrypt/live/$domain/privkey.pem" /etc/ssl/private/iRedMail.key
+ln -s "/etc/letsencrypt/live/$domain/fullchain.pem" /etc/ssl/certs/iRedMail.crt
 
 ## With base nginx config
 # sudo certbot --nginx --email $email -d $domain -d www.$domain
