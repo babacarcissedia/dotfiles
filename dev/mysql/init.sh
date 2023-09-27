9f744513a0a4d9b0e77620b833c263d2f3e9e5d8
Babacar Cissé Dia <bcdbuddy@outlook.com>
2023-09-27 09:07:29 -0400

    Update init.sh

--- dev/mysql/init.sh
+++ dev/mysql/init.sh
@@ -1,10 +1,11 @@
 name=$1
-root_password=${2:-'root'}
+username=${2:-'root'}
+password=${3:-'root'}
 echo '' > tmp.sql
 echo "CREATE USER $name@localhost identified with mysql_native_password by \"$name\";" >> tmp.sql
 echo "CREATE DATABASE $name charset utf8 collate utf8_general_ci;" >> tmp.sql
 echo "GRANT ALL PRIVILEGES ON $name.* to $name@localhost;" >> tmp.sql
-mysql -uroot -p$root_password -e "source tmp.sql"
+mysql -u$username -p$password -e "source tmp.sql"
 
 
-mysql -uroot -proot -e "CREATE DATABASE mmtrransport charset utf8 collate utf8_general_ci;"
+mysql -uroot -proot -e "CREATE DATABASE $name charset utf8 collate utf8_general_ci;"
