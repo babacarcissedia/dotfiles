9ce207a44741b55848db67ab3ddfb6fd7fa2d520
Babacar Cissé Dia <bcdbuddy@outlook.com>
2023-09-27 09:21:55 -0400

    Update non_ssl.sh

--- dev/nginx/non_ssl.sh
+++ dev/nginx/non_ssl.sh
@@ -1,6 +1,4 @@
 name=$1 # site.domain
-user=$(USER)
-php_version=${2:-'8.2'}
 webroot=${3:-'/var/www/html'}
 touch /etc/nginx/sites-available/$name.conf
 ln -s /etc/nginx/sites-available/$name.conf /etc/nginx/sites-enabled/$name.conf
@@ -9,7 +7,7 @@ cat >> /etc/nginx/sites-available/$name.conf << EOF
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/html;
+    root $webroot;
     server_name $name www.$name;
 }
 EOF
