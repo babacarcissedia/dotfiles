1f8188a0355901b593673a0458b0b10c0c6633c4
Babacar Cissé Dia <bcdbuddy@outlook.com>
2023-09-27 09:30:21 -0400

    Update non_ssl.sh

--- dev/nginx/non_ssl.sh
+++ dev/nginx/non_ssl.sh
@@ -1,5 +1,5 @@
 name=$1 # site.domain
-webroot=${3:-'/var/www/html'}
+webroot=${3:-'/var/www/vhosts/$name'}
 touch /etc/nginx/sites-available/$name.conf
 ln -s /etc/nginx/sites-available/$name.conf /etc/nginx/sites-enabled/$name.conf
 cat >> /etc/nginx/sites-available/$name.conf << EOF
