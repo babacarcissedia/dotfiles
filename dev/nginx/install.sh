e5596246571ad482cdfdce05a6cdaaa4fc6749c7
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-03-15 14:01:33 +0000

    Update install.sh

--- dev/nginx/install.sh
+++ dev/nginx/install.sh
@@ -1,4 +1,3 @@
 sudo apt-get remove -y apache
 sudo apt-get install -y nginx
-rm /etc/nginx/sites-available/default 
-rm /etc/nginx/sites-enabled/default 
+rm /etc/nginx/sites-available/default  /etc/nginx/sites-enabled/default 
