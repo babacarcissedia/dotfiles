df2a5dd52dd594033ec21f953b1de825203aafd6
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-05-30 19:04:20 +0000

    add supervisor.sh

---
+++ dev/supervisor.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+user=${0:-$(USER)}
+root_dir="/home/$user/www/"
+processes=4
+sudo apt get install -y supervisor
+cat >> /etc/supervisor/conf.d/laravel-worker.conf << EOF
+
+command=php ${root_dir}/artisan queue:work --sleep=3 --tries=3 --max-time=3600
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+user=${user}
+numprocs=${processes}
+redirect_stderr=true
+stdout_logfile=${root_dir}/storage/logs/worker.log
+stopwaitsecs=3600
+EOF
