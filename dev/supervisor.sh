2d190b7968f4154da7ea88863034021ff63691f1
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-07-02 16:15:00 -0400

    Update supervisor.sh

--- dev/supervisor.sh
+++ dev/supervisor.sh
@@ -4,7 +4,7 @@ root_dir="/home/$user/www/"
 processes=4
 sudo apt get install -y supervisor
 cat >> /etc/supervisor/conf.d/laravel-worker.conf << EOF
-
+process_name=%(program_name)s_%(process_num)02d
 command=php ${root_dir}/artisan queue:work --sleep=3 --tries=3 --max-time=3600
 autostart=true
 autorestart=true
