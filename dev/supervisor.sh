aa1b031996a117376db8a8d37988a4605e1535eb
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-08-17 20:51:17 -0400

    Update supervisor.sh

--- dev/supervisor.sh
+++ dev/supervisor.sh
@@ -2,8 +2,10 @@
 user=${0:-$(USER)}
 root_dir="/home/$user/www/"
 processes=4
-sudo apt get install -y supervisor
+sudo apt-get install -y supervisor
+
 cat >> /etc/supervisor/conf.d/laravel-worker.conf << EOF
+[program:laravel-worker]
 process_name=%(program_name)s_%(process_num)02d
 command=php ${root_dir}/artisan queue:work --sleep=3 --tries=3 --max-time=3600
 autostart=true
@@ -15,6 +17,24 @@ numprocs=${processes}
 redirect_stderr=true
 stdout_logfile=${root_dir}/storage/logs/worker.log
 stopwaitsecs=3600
+EOF
+
+
+cat >> /etc/supervisor/conf.d/scheduler.conf << EOF
+[program:laravel-scheduler]
+process_name=%(program_name)s_%(process_num)02d
+command=php artisan schedule:run
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+user=nginx
+numprocs=1
+redirect_stderr=true
+stopwaitsecs=3600
+stdout_logfile_maxbytes=100
+stderr_logfile=/dev/stderr
+stdout_logfile=/var/log/scheduler.log
 EOF
 
 sudo supervisorctl reread
