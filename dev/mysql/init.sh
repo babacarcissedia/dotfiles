c733bca670a96bdd387cc28991c2d4717ee6035a
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-08-25 14:11:48 -0400

    Update init.sh

--- dev/mysql/init.sh
+++ dev/mysql/init.sh
@@ -1,13 +1,9 @@
-name=$1
-username=${2:-$name}
-password=${3:-$name}
-root_username=${4:-'root'}
-root_password=${5:-''}
+# TODO: check variable exist
 echo '' > tmp.sql
-echo "CREATE USER $name@localhost identified with mysql_native_password by \"$password\";" >> tmp.sql
-echo "CREATE DATABASE $name charset utf8 collate utf8_general_ci;" >> tmp.sql
-echo "GRANT ALL PRIVILEGES ON $name.* to $name@localhost;" >> tmp.sql
-mysql -u$root_username -p$root_password -e "source tmp.sql"
+echo "CREATE USER $DB_USERNAME@$DB_HOST identified with mysql_native_password by \"$DB_PASSWORD\";" >> tmp.sql
+echo "CREATE DATABASE $DB_DATABASE charset utf8 collate utf8_general_ci;" >> tmp.sql
+echo "GRANT ALL PRIVILEGES ON $DB_DATABASE.* to $DB_USERNAME@$DB_HOST;" >> tmp.sql
+mysql -uroot -p -e "source tmp.sql"
 
 
 mysql -u$root_username -p$root_password -e "CREATE DATABASE $name charset utf8 collate utf8_general_ci;"
