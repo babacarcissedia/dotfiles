ff818a105682b02a3cf847ba9a9c7609e6c746aa
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-11-13 16:02:36 +0000

    Update laravel.sh

--- dev/nginx/laravel.sh
+++ dev/nginx/laravel.sh
@@ -128,14 +128,6 @@ server {
 }
 EOF
 
-# permissions
-sudo find storage -type f -exec chmod 664 {} \;
-sudo find storage -type d -exec chmod 775 {} \;
-sudo chmod -R ug+rwx storage bootstrap/cache
-sudo chgrp -R www-data storage bootstrap/cache
-sudo usermod -aG $APP_USER www-data
-sudo chown $APP_USER:www-data -R storage bootstrap/cache
-
 # Check file exists
 # /etc/ssl/dhparams.pem
 # Generate if not
