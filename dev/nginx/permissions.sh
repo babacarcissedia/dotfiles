58b9b13de903b840e17e48cac80aff100124f1b1
Babacar Cissé Dia <bcdbuddy@outlook.com>
2024-11-13 16:42:20 +0000

    Update permissions.sh

--- dev/nginx/permissions.sh
+++ dev/nginx/permissions.sh
@@ -1,3 +1,5 @@
+mkdir -p storage/framework/{sessions,views,cache}
+chmod -R 775 storage/framework
 sudo find storage -type f -exec chmod 664 {} \;
 sudo find storage -type d -exec chmod 775 {} \;
 sudo chmod -R ug+rwx storage bootstrap/cache
